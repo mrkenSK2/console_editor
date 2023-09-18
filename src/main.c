894a1993d3b3582af814b51d661bb96a3628c02f
mrkenSK2 <nakamura.k.cb@m.titech.ac.jp>
2023-09-18 16:52:44 +0900

    ctrl Q

--- src/main.c
+++ src/main.c
@@ -575,6 +575,7 @@ struct command command_parse(mbchar key) {
     static enum ControlKeyFlag flag = 0;
     struct command cmd;
     cmd.command_key = NONE;
+    cmd.command_value = key;
     if (key[0] == 0x1B && flag == NOT_CTRL)
         flag = ALLOW_1;
     else if (key[0] == 0x5B && flag == ALLOW_1)
@@ -582,22 +583,20 @@ struct command command_parse(mbchar key) {
     else if (flag == ALLOW_2) {
         if (key[0] == 0x41)
             cmd.command_key = UP;
-        if (key[0] == 0x42)
+        else if (key[0] == 0x42)
             cmd.command_key = DOWN;
-        if (key[0] == 0x43)
+        else if (key[0] == 0x43)
             cmd.command_key = RIGHT;
-        if(key[0] == 0x44)
+        else if(key[0] == 0x44)
             cmd.command_key = LEFT;
         flag = NOT_CTRL;
     } else {
+        if (key[0] == 0x11)
+            cmd.command_key = EXIT;
+        else
+            cmd.command_key = INSERT;
         flag = NOT_CTRL;
-        cmd.command_key = INSERT;
-    }
-    cmd.command_value = key;
-
-    if (!strcmp((const char*)key, "e"))
-        cmd.command_key = EXIT;
-        
+    }        
     return cmd;
 }
 
